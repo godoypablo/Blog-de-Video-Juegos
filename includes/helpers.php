ef8c41d5bbf058d60ae3081ee5f25085d7e74ef2
pablo <godoypablo@gmail.com>
2019-01-15 16:26:34 -0300

    Mi primer commit

---
+++ includes/helpers.php
@@ -0,0 +1,34 @@
+<?php
+function mostrarError($errores, $campo){
+    $alerta = '';
+    if (isset($errores[$campo]) && !empty($campo)){
+        $alerta = "<div class='alerta alerta-error'>".$errores[$campo].'</div>';
+    }
+    return $alerta;
+}
+
+function borrarErrores(){
+    $borrado = false;
+    if(isset($_SESSION['errores'])){
+    $_SESSION['errores'] = null;
+    $borrado = session_unset($_SESSION['errores']);
+    }    
+    
+    if(isset($_SESSION['completado'])){
+       $_SESSION['completado'] = null;
+       session_unset($_SESSION['completado']);
+    }
+    
+    return $borrado;
+}
+
+function conseguirCategorias($conexion){
+    $sql = "SELECT * FROM categorias ORDER BY id ASC;";
+    $categorias = mysqli_query($conexion, $sql);
+    
+    $result = array(); // por defecto trae un arreglo vacío
+    if($categorias && mysqli_num_rows($categorias) >= 1){
+        $result = $categorias;
+    }
+    return $result;
+}
---
+++ index.php
@@ -0,0 +1,57 @@
+<?php require_once 'includes/cabecera.php'; ?>
+<?php require_once 'includes/lateral.php'; ?>
+
+<!-- CAJA PRINCIPAL --> 
+<div id="principal">
+
+    <h1>Ultimas entradas</h1>
+    <article class="entrada">
+        <a href="">
+            <h2>Titulo de mi entrada</h2>
+            <p>
+                Lorem Ipsum
+                "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."
+                "No hay nadie que ame el dolor mismo, que lo busque, lo encuentre y lo quiera, simplemente porque es el dolor."
+            </p>
+        </a>
+    </article>
+
+    <article class="entrada">
+        <a href="">
+            <h2>Titulo de mi entrada</h2>
+            <p>
+                Lorem Ipsum
+                "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."
+                "No hay nadie que ame el dolor mismo, que lo busque, lo encuentre y lo quiera, simplemente porque es el dolor."
+            </p>
+        </a>
+    </article>
+
+    <article class="entrada">
+        <a href="">
+            <h2>Titulo de mi entrada</h2>
+            <p>
+                Lorem Ipsum
+                "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."
+                "No hay nadie que ame el dolor mismo, que lo busque, lo encuentre y lo quiera, simplemente porque es el dolor."
+            </p>
+        </a>
+    </article>
+
+    <article class="entrada">
+        <a href="">
+            <h2>Titulo de mi entrada</h2>
+            <p>
+                Lorem Ipsum
+                "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."
+                "No hay nadie que ame el dolor mismo, que lo busque, lo encuentre y lo quiera, simplemente porque es el dolor."
+            </p>
+        </a>
+    </article>
+
+    <div id="ver-todas">
+        <a href="">Ver todas las entradas</a>
+    </div>
+</div> <!-- Fin Principal -->
+
+<?php require_once 'includes/pie.php'; ?>
